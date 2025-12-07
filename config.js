81edb71157054a72747732c1e7a0fb286f769eb0
jcnoga <164394666+jcnoga@users.noreply.github.com>
2025-12-06 21:14:09 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+// config.js
+const firebaseConfig = {
+    apiKey: "SUA_API_KEY_AQUI",
+    authDomain: "seu-projeto.firebaseapp.com",
+    projectId: "seu-projeto",
+    storageBucket: "seu-projeto.appspot.com",
+    messagingSenderId: "...",
+    appId: "..."
+};
+
+// Exporta para ser usado no HTML
+export { firebaseConfig };
